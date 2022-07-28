4a0c753d1ead0f75221acefc3f7d6bca4d888916
katemayorova <katemayorova0@gmail.com>
2022-07-28 19:04:08 +0300

    Created tag sorting in the class model Scope

--- articles/models.py
+++ articles/models.py
@@ -5,19 +5,7 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-
-
-class Tag(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=50, verbose_name='Название тега')
-
-
-class Scope(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    article = models.ForeignKey(Article, related_name='scopes', on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
-    is_main = models.BooleanField(default=False)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
 
     class Meta:
         verbose_name = 'Статья'
@@ -25,3 +13,30 @@ class Scope(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50, verbose_name='Название')
+    articles = models.ManyToManyField(Article, through='Scope')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Разделы')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+        ordering = ('-is_main', 'tag__name')
+
+
